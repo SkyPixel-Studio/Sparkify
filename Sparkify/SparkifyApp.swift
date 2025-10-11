55fdafad966c6a0b5b8cc935a87fcd31cc5c2e4a
ringotypowriter <ringotypewriter@qq.com>
2025-10-11 22:15:21 +0800

    Initial Commit

---
+++ Sparkify/SparkifyApp.swift
@@ -0,0 +1,32 @@
+//
+//  SparkifyApp.swift
+//  Sparkify
+//
+//  Created by Willow Zhang on 2025/10/11.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SparkifyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
